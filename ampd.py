cd82215a585b6e7608a4314f1e7d33746ef84696
Igor Gotlibovych <igotlibovych@jawbone.com>
2018-02-28 16:19:50 +0000

    handle peaks near edges

--- ampd.py
+++ ampd.py
@@ -48,3 +48,53 @@ def find_peaks_ampd(x, scale=None, debug=False):
     if debug:
         return pks, LSM, l
     return pks
+
+
+def find_peaks_ampd_v2(x, scale=None, debug=False):
+    """Find peaks in quasi-periodic noisy signals using AMPD algorithm
+    Automati Multi-Scale Peak Detection originally proposed in
+    "An Efficient Algorithm for Automatic Peak Detection in
+    Noisy Periodic and Quasi-Periodic Signals", Algorithms 2012, 5, 588-603
+    https://doi.org/10.1109/ICRERA.2016.7884365
+
+    Optimized implementation by Igor Gotlibovych, 2018
+
+
+    Parameters
+    ----------
+    x : ndarray
+        1-D array on which to find peaks
+    scale : int, optional
+        specify maximum scale window size of (2 * scale + 1)
+    debug : bool, optional
+        if set to True, the Local Scalogram Matrix, `LSM`, and scale with most local maxima, `l`,
+        are returned together with peak locations
+
+    Returns
+    -------
+    pks: ndarray
+        The ordered array of peak indices found in `x`
+    """
+    x = detrend(x)
+    N = len(x)
+    L = N // 2
+    if scale:
+        L = min(scale, L)
+
+    # create LSM matix
+    LSM = np.ones((L, N), dtype=bool)
+    for k in np.arange(1, L+1):
+        LSM[k-1, 0:N-k] &= (x[0:N-k] > x[k:N])  # compare to right neighbours
+        LSM[k-1, k:N] &= (x[k:N] > x[0:N-k])  # compare to left neighbours
+        
+    # Find scale with most maxima
+    G = LSM.sum(axis=1)
+    G = G * np.arange(N//2, N//2-L, -1)  # normalize to adjust for new edge regions
+    l = np.argmax(G)
+
+    # find peaks that persist on all scales up to l
+    pks_logical = np.min(LSM[0:l, :], axis=0)
+    pks = np.nonzero(pks_logical)
+    if debug:
+        return pks, LSM, G, l
+    return pks
